614b42702d5322cbdc58dbd022e81a2a063f45f5
just-seba <46223600+just-seba@users.noreply.github.com>
2024-05-29 08:03:37 +0200

    uses vogen generated schema filter.

--- server/src/Emma.Server/Swagger/SwaggerGenServiceCollectionExtensions.cs
+++ server/src/Emma.Server/Swagger/SwaggerGenServiceCollectionExtensions.cs
@@ -23,7 +23,7 @@ public static class SwaggerGenServiceCollectionExtensions
             );
 
             options.SupportNonNullableReferenceTypes();
-            options.SchemaFilter<ValueObjectSchemaFilter>();
+            options.SchemaFilter<VogenSchemaFilter>();
             options.SchemaFilter<RequireNonNullablePropertiesSchemaFilter>();
             options.ParameterFilter<RequireNonNullableParameterFilter>();
 
