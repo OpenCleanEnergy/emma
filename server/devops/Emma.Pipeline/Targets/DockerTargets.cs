90c55a98021d528aee0c53bda90be05cc5ce564b
just-seba <46223600+just-seba@users.noreply.github.com>
2024-06-03 20:26:32 +0200

    fix: fixes multi-platform build /6

--- server/devops/Emma.Pipeline/Targets/DockerTargets.cs
+++ server/devops/Emma.Pipeline/Targets/DockerTargets.cs
@@ -8,7 +8,7 @@ using static SimpleExec.Command;
 public static class DockerTargets
 {
     public const string Tags = "docker:tags";
-    public const string Pack = "docker:pack";
+    public const string Build = "docker:build";
     public const string Publish = "docker:publish";
 
     public static Targets AddDockerTargets(this Targets targets)
@@ -19,10 +19,9 @@ public static class DockerTargets
         targets.Add(
             Tags,
             "Prints the docker tags. The registry is read from $DOCKER_REGISTRY; defaults to 'ghcr.io'.",
-            ForEach(dockerBaseImage),
-            (baseImage) =>
+            () =>
             {
-                foreach (var tag in DockerTags.GetTags(baseImage))
+                foreach (var tag in DockerTags.GetTags(dockerBaseImage))
                 {
                     Console.WriteLine($"🏷️ {tag}");
                 }
@@ -30,27 +29,30 @@ public static class DockerTargets
         );
 
         targets.Add(
-            Pack,
+            Build,
             "Builds the docker image.",
-            DependsOn(Tags),
-            ForEach(dockerBaseImage),
-            (baseImage) =>
+            dependsOn: [Tags],
+            () =>
             {
-                var tags = DockerTags.GetTags(baseImage);
+                var tags = DockerTags.GetTags(dockerBaseImage);
                 var tagArgs = string.Join(" ", tags.Select(tag => $"--tag {tag}"));
-                return RunAsync(
-                    "docker",
-                    $"buildx build --load --platform linux/amd64,linux/arm64 {tagArgs} ."
-                );
+                return RunAsync("docker", $"build {tagArgs} .");
             }
         );
 
         targets.Add(
             Publish,
-            "Pushes the docker image to the registry. The registry is read from $DOCKER_REGISTRY; defaults to 'ghcr.io'.",
-            DependsOn(Pack),
-            ForEach(DockerTags.GetTags(dockerBaseImage).ToArray()),
-            (tag) => RunAsync("docker", $"push {tag}")
+            "Uses buildx to build and publish a multi-platform image.",
+            dependsOn: [Build],
+            () =>
+            {
+                var tags = DockerTags.GetTags(dockerBaseImage);
+                var tagArgs = string.Join(" ", tags.Select(tag => $"--tag {tag}"));
+                return RunAsync(
+                    "docker",
+                    $"buildx build --platform linux/amd64,linux/arm64 {tagArgs}"
+                );
+            }
         );
 
         return targets;
