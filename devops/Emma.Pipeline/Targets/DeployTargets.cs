a2722ba0f8925df96696cbd1a10499cb98217f31
Sebastian Rumohr <46223600+just-seba@users.noreply.github.com>
2024-06-04 11:57:04 +0200

    uses Bullseye's `forEach` to render multiple templates.

--- devops/Emma.Pipeline/Targets/DeployTargets.cs
+++ devops/Emma.Pipeline/Targets/DeployTargets.cs
@@ -5,17 +5,18 @@ using Emma.Pipeline.Targets.Deploy;
 
 public static class DeployTargets
 {
-    public const string ServerSecrets = "deploy:server-secrets";
+    public const string Templates = "deploy:templates";
 
     public static Targets AddDeployTargets(this Targets targets)
     {
         targets.Add(
-            ServerSecrets,
+            Templates,
             "Substitutes environment variables in ./devops/templates/server/secrets.json",
-            () =>
+            forEach: ["server/secrets.json", "keycloak/.env", "keycloak/realm.json"],
+            (template) =>
                 EnvSubst.Substitute(
-                    new FileInfo("./devops/templates/server/secrets.json"),
-                    new FileInfo("./devops/rendered/server/secrets.json")
+                    new FileInfo($"./devops/templates/{template}"),
+                    new FileInfo($"./devops/rendered/{template}")
                 )
         );
 
