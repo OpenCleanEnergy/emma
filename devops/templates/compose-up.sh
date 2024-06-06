9ab971cae06d87c3f63912a0b94053f2739a9259
Sebastian Rumohr <46223600+just-seba@users.noreply.github.com>
2024-06-06 11:28:02 +0200

    adds script to start or compose stack

---
+++ devops/templates/compose-up.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+set -euo pipefail
+
+echo '📥 Pull latest images'
+docker compose pull
+
+echo '🚀 Create and start containers'
+docker compose up --detach --remove-orphans
+
+echo '⏳ Wait for containers to start'
+sleep 5s
+
+echo '♻ Reload caddy configuration'
+# https://caddyserver.com/docs/running#usage
+CADDY_ID=$(docker container ps --quiet --filter 'name=proxy')
+docker exec --workdir /etc/caddy/ ${CADDY_ID} caddy reload
+
+echo '👪 Configure keycloak'
+docker run --rm \
+  --env-file ./keycloak/config/.env \
+  --volume ./keycloak/config:/config \
+  quay.io/keycloak-config-cli:latest-24.0.1
