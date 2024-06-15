e607f6b698a0b82de73465423fb6c4dac69bda23
just-seba <46223600+just-seba@users.noreply.github.com>
2024-06-15 10:44:41 +0200

    adds screen to view logs.

---
+++ app/lib/application/logs/logs_store.dart
@@ -0,0 +1,22 @@
+import 'package:logging/logging.dart';
+import 'package:signals/signals.dart';
+import 'package:signals/signals_flutter.dart';
+
+class LogsStore {
+  static final LogsStore _instance = LogsStore._();
+
+  LogsStore._();
+
+  static LogsStore get instance => _instance;
+
+  final ListSignal<LogRecord> records = listSignal([]);
+
+  void addLog(LogRecord record) {
+    batch(() {
+      records.add(record);
+      if (records.length > 25) {
+        records.removeAt(0);
+      }
+    });
+  }
+}
---
+++ app/lib/ui/logs/logs_screen.dart
@@ -0,0 +1,44 @@
+import 'package:emma/application/logs/logs_store.dart';
+import 'package:flutter/material.dart';
+import 'package:signals/signals_flutter.dart';
+
+class LogsScreen extends StatefulWidget {
+  const LogsScreen({super.key});
+
+  @override
+  State<LogsScreen> createState() => _LogsScreenState();
+}
+
+class _LogsScreenState extends State<LogsScreen> {
+  final records = LogsStore.instance.records;
+
+  @override
+  Widget build(BuildContext context) {
+    const style = TextStyle(fontFamily: 'monospace');
+
+    return Scaffold(
+        appBar: AppBar(title: const Text("Logs")),
+        body: Watch(
+          (context) => ListView.separated(
+              padding: const EdgeInsets.all(8.0),
+              itemBuilder: (context, index) {
+                final record = records[records.length - 1 - index];
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(record.time.toIso8601String(), style: style),
+                    Text(record.toString(), style: style),
+                    if (record.error != null)
+                      Text(
+                        record.error.toString(),
+                        style: style.copyWith(
+                            color: Theme.of(context).colorScheme.error),
+                      )
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => const Divider(),
+              itemCount: records.length),
+        ));
+  }
+}
