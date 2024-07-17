5d7cb56f95b9fdf9948963cc54e6161bed6af036
just-seba <46223600+just-seba@users.noreply.github.com>
2024-07-17 14:17:20 +0200

    fixes transaction behavior.
    `autoCommit: true` only works if `ICapPublisher` is used to publish events. If there are no events to publish, the transaction is not committed.

--- server/src/Emma.Application.Shared/RequiresTransactionAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace Emma.Application.Shared;
-
-[AttributeUsage(
-    AttributeTargets.Class | AttributeTargets.Interface,
-    Inherited = true,
-    AllowMultiple = false
-)]
-public sealed class RequiresTransactionAttribute : Attribute;
