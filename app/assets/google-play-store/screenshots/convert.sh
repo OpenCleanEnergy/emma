fc0c26a23eef32f1683d48b6206d6d2a36a7f94d
just-seba <46223600+just-seba@users.noreply.github.com>
2024-08-25 06:56:58 +0200

    chore: fixes phone scaling issue in play store listing images.
    chore: uses `bigjpg.com` to scale images`

--- app/assets/google-play-store/screenshots/convert.sh
+++ app/assets/google-play-store/screenshots/convert.sh
@@ -7,9 +7,5 @@ for src in 0-raw/*.png; do
   CROP='1-crop'
   mkdir -p $CROP
   magick $src -crop +26-29 -crop -26+70 $CROP/$NAME
-
-  SCALE='2-scale'
-  mkdir -p $SCALE
-  magick $CROP/$NAME -scale 200% $SCALE/$NAME
 done
 
