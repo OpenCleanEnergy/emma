e35cb2b85d345b5d50e6ed3562f4b2a07b8254d6
Sebastian Rumohr <46223600+just-seba@users.noreply.github.com>
2024-09-02 08:04:27 +0200

    feat: Improve status indicator on home screen. (#191)

--- app/lib/ui/home/status/status_indicator.dart
+++ app/lib/ui/home/status/status_indicator.dart
@@ -2,11 +2,8 @@ import 'package:emma/ui/shared/unit_text.dart';
 import 'package:flutter/material.dart';
 import 'package:signals/signals_flutter.dart';
 
-class StatusIndicator extends StatelessWidget {
-  static const double _indicatorSize = 96;
-  static const double _iconSize = 32;
-
-  StatusIndicator({
+class StatusIndicator extends StatefulWidget {
+  const StatusIndicator({
     super.key,
     required this.icon,
     required this.value,
@@ -19,22 +16,42 @@ class StatusIndicator extends StatelessWidget {
   final ReadonlySignal<double>? maxValue;
   final String unit;
 
+  @override
+  State<StatusIndicator> createState() => _StatusIndicatorState();
+}
+
+class _StatusIndicatorState extends State<StatusIndicator> {
+  static const double _indicatorSize = 96;
+  static const double _iconSize = 32;
+
   late final _indicatorValue = computed(() {
-    if (value.value == 0) {
-      return 0;
-    } else if (maxValue == null || maxValue!.value == 0) {
-      return 1;
-    } else if (maxValue!.value < value.value) {
-      return 1;
+    final value = widget.value.value.round();
+    final maxValue = widget.maxValue?.value.round() ?? 0;
+
+    if (value == 0) {
+      return 0.0;
+    } else if (maxValue == 0) {
+      return 1.0;
+    } else if (maxValue < value) {
+      return 1.0;
     } else {
-      return value.value / maxValue!.value;
+      final fraction = value / maxValue;
+      // Round to 2 decimal places
+      return (fraction * 100).round() / 100;
     }
   });
 
   late final _tween = computed(() => Tween<double>(
-        begin: _indicatorValue.previousValue?.toDouble() ?? 0.0,
-        end: _indicatorValue.value.toDouble(),
+        begin: _indicatorValue.previousValue ?? 0.0,
+        end: _indicatorValue.value,
       ));
+
+  @override
+  void dispose() {
+    _indicatorValue.dispose();
+    _tween.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,8 +61,8 @@ class StatusIndicator extends StatelessWidget {
         Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Watch((context) => Icon(icon.value, size: _iconSize)),
-            Watch((context) => UnitText(value.value, unit)),
+            Watch((context) => Icon(widget.icon.value, size: _iconSize)),
+            Watch((context) => UnitText(widget.value.value, widget.unit)),
           ],
         ),
         SizedBox.square(
@@ -68,5 +85,3 @@ class StatusIndicator extends StatelessWidget {
     );
   }
 }
-
-enum StatusIndicatorDirection { none, up, down }
