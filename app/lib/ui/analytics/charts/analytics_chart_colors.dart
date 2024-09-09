cc2d26cfd00393453c515a0e62713a2ab5b1878e
just-seba <46223600+just-seba@users.noreply.github.com>
2024-09-09 14:28:13 +0200

    use material color.

--- app/lib/ui/analytics/charts/analytics_chart_colors.dart
+++ app/lib/ui/analytics/charts/analytics_chart_colors.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 extension AnalyticsChartColors on Color {
-  static Color get production => CupertinoColors.systemYellow;
-  static Color get home => CupertinoColors.systemBlue;
-  static Color get gridConsumption => CupertinoColors.systemRed;
-  static Color get gridFeedIn => CupertinoColors.systemGreen;
+  static Color get production => Colors.amber;
+  static Color get home => Colors.blue;
+  static Color get gridConsumption => Colors.red;
+  static Color get gridFeedIn => Colors.green;
 
   Color toBarAreaColor() => withOpacity(0.067);
 }
