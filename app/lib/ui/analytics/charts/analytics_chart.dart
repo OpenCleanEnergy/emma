699d81227197f070c1344001269dda90efeb8b76
just-seba <46223600+just-seba@users.noreply.github.com>
2024-09-09 08:35:35 +0200

    deactivates not implemented charts

--- app/lib/ui/analytics/charts/analytics_chart.dart
+++ app/lib/ui/analytics/charts/analytics_chart.dart
@@ -1,7 +1,9 @@
+import 'package:emma/ui/analytics/analytics_period.dart';
 import 'package:emma/ui/analytics/analytics_view_model.dart';
 import 'package:emma/ui/analytics/charts/analytics_chart_control.dart';
 import 'package:emma/ui/analytics/charts/analytics_day_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:signals/signals_flutter.dart';
 
 class AnalyticsChart extends StatelessWidget {
   const AnalyticsChart({super.key, required this.viewModel});
@@ -18,7 +20,17 @@ class AnalyticsChart extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          AnalyticsDayChart(viewModel: viewModel),
+          Watch(
+            (context) => switch (viewModel.period.value) {
+              AnalyticsPeriod.day => AnalyticsDayChart(viewModel: viewModel),
+              _ => const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Center(
+                    child: Text("Bald verfügbar."),
+                  ),
+                ),
+            },
+          ),
           const SizedBox(height: 8),
           AnalyticsChartControl(viewModel: viewModel)
         ],
